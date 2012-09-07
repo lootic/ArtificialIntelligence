7ca6eff222b1308a538c094121afded984a870b6
Jonathan Lundholm <lootic@hotmail.com>
2012-09-07 12:08:45 +0200

    Commited finished code for lab2

---
+++ Lab2/lab2_part1/src/searchCustom/CustomDepthFirstSearch.java
@@ -0,0 +1,10 @@
+package searchCustom;
+
+import java.util.Random;
+
+
+public class CustomDepthFirstSearch extends CustomGraphSearch{
+	public CustomDepthFirstSearch(int maxDepth){
+		super(true); // Temporary random choice, you need to true or false!
+		System.out.println("Change line above in \"CustomDepthFirstSearch.java\"!");}
+};
